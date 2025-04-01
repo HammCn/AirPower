66c9a7a68c30c92307f50895d66330782eccea56
Hamm <majhamm@qq.com>
2025-04-01 11:42:47 +0800

    fix(bugs): 修复了一些已知的BUG

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'airpower',
+      formats: ['es'],
       fileName: format => `airpower.${format}.js`,
     },
     rollupOptions: {
