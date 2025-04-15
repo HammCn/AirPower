ee2363f96a69096fd120cd2c59eec2cf2c46da2e
Hamm <majhamm@qq.com>
2025-04-15 10:56:25 +0800

    refactor(tree): 优化 ITree 接口定义
    - 移除了 name 属性，因为该信息在树结构中并非必须
    - 将 parentId 属性从可选改为必选，以简化数据结构

--- src/tree/ITree.ts
+++ src/tree/ITree.ts
@@ -7,11 +7,6 @@ import type { IEntity } from '../base'
  */
 export interface ITree extends IEntity {
   /**
-   * ### 树节点名称
-   */
-  name: string
-
-  /**
    * ### 树的子节点
    * 为了成功的数据转换,请注意自行 `@Type`
    */
@@ -20,5 +15,5 @@ export interface ITree extends IEntity {
   /**
    * ### 父节点 `ID`
    */
-  parentId?: number
+  parentId: number
 }
